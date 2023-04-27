4a13a655ab9004a43b380dd7b0baac7369b139ab
yasantha <niroshanyi@gmail.com>
2023-04-27 15:10:55 +0530

    completed lab 07

--- Lab 07/BST.cpp
+++ Lab 07/BST.cpp
@@ -7,86 +7,109 @@ struct node
     struct node *left, *right;
 };
 
-// Inorder traversal
 void traverseInOrder(struct node *root)
 {
     if (root != NULL)
     {
-        // call
+
+        // recursing trogh left subtree untill we reach the leaf node
         traverseInOrder(root->left);
+
+        // printing the value of the leaf node
         cout << root->key << " ";
+
+        // recursing through right subtree untill we reach the leaf node
         traverseInOrder(root->right);
     }
 }
 
-// Insert a node
+// Function to insert a new node with given key in BST
 struct node *insertNode(struct node *node, int key)
-{
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+{   
+    // if the node is empty then we create a new node and return it as root
     if (node == NULL)
     {
+        struct node *new_node = (struct node *)malloc(sizeof(struct node));
         new_node->key = key;
         new_node->left = NULL;
         new_node->right = NULL;
         return new_node;
     }
+
+    // if the node is not empty then we check if the key is less than the node's key
     else if (key < node->key)
     {
+        //recurse through the left subtree
         node->left = insertNode(node->left, key);
     }
     else if (key > node->key)
     {
+        //recurse through the right subtree
         node->right = insertNode(node->right, key);
+    }
+    else
+    {
+        // if the key is equal to the node's key then we return the node
+        return node;
     }
     return node;
 }
 
-// Deleting a node
+// Function to delete a node with given key from BST
 struct node *deleteNode(struct node *root, int key)
-{
-    if(root == NULL)
+{   
+    // if node is empty then we return the node
+    if (root == NULL)
     {
         return root;
     }
+
+    // if the key is less than the node's key then we recurse through the left subtree
     else if (key < root->key)
     {
         root->left = deleteNode(root->left, key);
     }
+    // if the key is greater than the node's key then we recurse through the right subtree
     else if (key > root->key)
     {
         root->right = deleteNode(root->right, key);
     }
+    // if the key is equal to the node's key then we delete the node after checking whether how many children the node has
     else
     {
+        // if the node has no children then we delete the node
         if (root->left == NULL && root->right == NULL)
         {
             free(root);
             root = NULL;
         }
+        // if the node has only one child in right then we delete the node and replace it with its child
         else if (root->left == NULL)
         {
-            struct node *temp = root;
+            struct node *new_node = root;
             root = root->right;
-            free(temp);
+            free(new_node);
         }
+        // if the node has only one child in left then we delete the node and replace it with its child
         else if (root->right == NULL)
         {
-            struct node *temp = root;
+            struct node *new_node = root;
             root = root->left;
-            free(temp);
+            free(new_node);
         }
+        // if the node has two children then we find the minimum in the right subtree and replace it with the node key and delete the minimum node
         else
         {
-            struct node *temp = root->right;
-            while (temp->left != NULL)
+            struct node *new_node = root->right;
+            while (new_node->left != NULL)
             {
-                temp = temp->left;
+                new_node = new_node->left;
             }
-            root->key = temp->key;
-            root->right = deleteNode(root->right, temp->key);
+            root->key = new_node->key;
+            root->right = deleteNode(root->right, new_node->key);
         }
-        return root;
     }
+    return root;
 }
 
 // Driver code
