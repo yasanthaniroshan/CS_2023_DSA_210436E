99ad1190474509fbf83e88ccecc19538d98f47ed
yasantha <niroshanyi@gmail.com>
2023-04-27 13:45:22 +0530

    initialize lab 07

---
+++ Lab 07/BST.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+  int key;
+  struct node *left, *right;
+};
+
+// Inorder traversal
+void traverseInOrder(struct node *root) {
+
+}
+
+// Insert a node
+struct node *insertNode(struct node *node, int key) {
+
+}
+
+// Deleting a node
+struct node *deleteNode(struct node *root, int key) {
+ 
+}
+
+// Driver code
+int main() {
+  struct node *root = NULL;
+
+  int operation;
+  int operand;
+  cin >> operation;
+
+  while (operation != -1) {
+    switch(operation) {
+      case 1: // insert
+        cin >> operand;
+        root = insertNode(root, operand);
+        cin >> operation;
+        break;
+      case 2: // delete
+        cin >> operand;
+        root = deleteNode(root, operand);
+        cin >> operation;
+        break;
+      default:
+        cout << "Invalid Operator!\n";
+        return 0;
+    }
+  }
+  
+  traverseInOrder(root);
+}
