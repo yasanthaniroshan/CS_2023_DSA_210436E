70e262cff66bb4122b76f1a6d427ec7e08970706
yasantha <niroshanyi@gmail.com>
2023-04-27 14:10:21 +0530

    completed node insertion function

--- Lab 07/BST.cpp
+++ Lab 07/BST.cpp
@@ -8,17 +8,41 @@ struct node {
 
 // Inorder traversal
 void traverseInOrder(struct node *root) {
-
+    if(root != NULL)
+    {
+        cout << root->key << " ";
+    }
+    traverseInOrder(root->left);
+    cout << root->key << " ";
+    traverseInOrder(root->right);
 }
 
 // Insert a node
 struct node *insertNode(struct node *node, int key) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    if(node == NULL)
+    {
+        new_node->key = key;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        return new_node;
+    }
+    else if (key < node->key)
+    {
+       node->left = insertNode(node->left,key);
+    }
+    else if(key > node->key)
+    {
+        node->right = insertNode(node->right,key);
 
+    }
+    return node;
 }
 
 // Deleting a node
 struct node *deleteNode(struct node *root, int key) {
  
+
 }
 
 // Driver code
